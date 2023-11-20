5653f7231d6a0f05f19790b5d9882330b82e6405
Yingying Lu <20095278@mail.wit.ie>
2023-11-20 11:56:12 +0000

    Change and refactor the movie details.

--- movies/src/components/movieDetails/index.js
+++ movies/src/components/movieDetails/index.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import Chip from "@mui/material/Chip";
-import Paper from "@mui/material/Paper";
-import AccessTimeIcon from "@mui/icons-material/AccessTime";
-import MonetizationIcon from "@mui/icons-material/MonetizationOn";
-import StarRate from "@mui/icons-material/StarRate";
 import NavigationIcon from "@mui/icons-material/Navigation";
 import Fab from "@mui/material/Fab";
-import Typography from "@mui/material/Typography";
 import Drawer from "@mui/material/Drawer";
-import MovieReviews from "../movieReviews"
+import MovieReviews from "../movieReviews";
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
+import Stack from '@mui/material/Stack';
+import MovieFilterIcon from '@mui/icons-material/MovieFilter';
+import Grid from "@mui/material/Grid";
 
 
 const root = {
@@ -26,52 +27,51 @@ const MovieDetails = ({ movie }) => {  // Don't miss this!
 
   return (
     <>
-      <Typography variant="h5" component="h3">
-        Overview
-      </Typography>
+      <div style={{ 
+        background: `url(https://image.tmdb.org/t/p/w500/${movie.backdrop_path})`, 
+        backgroundSize: 'cover',
+        width: '100%',
+        height: '600px',
+      }}>
+      </div>
 
-      <Typography variant="h6" component="p">
-        {movie.overview}
-      </Typography>
+      <Container style={{marginTop: '2rem'}}>
+      <Box sx={{ width: '100%' }}>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={6}> 
+          <p style={{fontSize: "3rem", margin: '0', fontWeight: 'bold'}}>{movie.title}</p>
+          <p style={{marginLeft: '5%', fontSize: '1.5rem'}}>{movie.tagline?'-':''} {movie.tagline}</p>
+        <Stack>
+        <Stack style={{marginTop: '2rem'}} direction="row" spacing={1}>
+          <MovieFilterIcon />
+          <p>{movie.release_date}</p>
+          <p style={{textTransform:'uppercase'}}>({movie.original_language})</p>
+          <p>·</p>
+          <p>{movie.production_countries[0].iso_3166_1}</p>
+          <p>·</p>
+          <p>{movie.runtime} minutes</p>
+        </Stack>
+        <Stack style={{listStyle: 'none', marginTop: '1rem'}} direction="row" >
+          {movie.genres.map((g) => (
+            <li key={g.name}>
+              <Chip label={g.name} sx={{...chip}} />
+            </li>
+          ))}
+        </Stack>
+        </Stack>
 
-      <Paper 
-        component="ul" 
-        sx={{...root}}
-      >
-        <li>
-          <Chip label="Genres" sx={{...chip}} color="primary" />
-        </li>
-        {movie.genres.map((g) => (
-          <li key={g.name}>
-            <Chip label={g.name} sx={{...chip}} />
-          </li>
-        ))}
-      </Paper>
-      <Paper component="ul" sx={{...root}}>
-        <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} />
-        <Chip
-          icon={<MonetizationIcon />}
-          label={`${movie.revenue.toLocaleString()}`}
-        />
-        <Chip
-          icon={<StarRate />}
-          label={`${movie.vote_average} (${movie.vote_count}`}
-        />
-        <Chip label={`Released: ${movie.release_date}`} />
-      </Paper>
-      <Paper 
-        component="ul" 
-        sx={{...root}}
-      >
-        <li>
-          <Chip label="Production Countries" sx={{...chip}} color="primary" />
-        </li>
-        {movie.production_countries.map((g) => (
-          <li key={g.name}>
-            <Chip label={g.name} sx={{...chip}} />
-          </li>
-        ))}
-      </Paper>
+        <Stack style={{marginTop: '1rem'}} spacing={1}>
+          <Rating name="half-rating-read" defaultValue={(movie.vote_average)/2} precision={0.5} readOnly />
+        </Stack>  
+        <Stack>
+          <p style={{fontSize: '2rem', marginBottom: '0'}}>Overview</p>
+          <p style={{fontSize: '1.2rem'}}>{movie.overview}</p>
+        </Stack>
+
+        </Grid>
+      </Grid>
+    </Box>
+    </Container>
       <Fab
         color="secondary"
         variant="extended"
