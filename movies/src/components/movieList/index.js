4cbcbf76794121d852fb29fa0f7f0ee3071ac3b3
Yingying Lu <20095278@mail.wit.ie>
2023-11-25 03:29:57 +0000

    Add pagination.

--- movies/src/components/movieList/index.js
+++ movies/src/components/movieList/index.js
@@ -1,14 +1,49 @@
-import React from "react";
+import React,{ useState} from "react";
 import Movie from "../movieCard";
 import Grid from "@mui/material/Grid";
+import ReactPaginate from "react-paginate";
+import { Stack } from "@mui/material";
 
 const MovieList = ( {movies, action }) => {
-  let movieCards = movies.map((m) => (
+  const [pageNumber, setPageNumber] = useState(0);
+  const moviesPerPage = 10;
+  const pagesVisited = pageNumber*moviesPerPage;
+  const movieCards = movies.slice(pagesVisited, pagesVisited + moviesPerPage).map((m) => {
+    return (
     <Grid key={m.id} item xs={12} sm={6} md={4} lg={3} xl={2}>
       <Movie key={m.id} movie={m} action={action} />
     </Grid>
-  ));
-  return movieCards;
+    );
+  });
+  const pageCount = Math.ceil(movies.length / moviesPerPage);
+  const handlePageClick = ({selected}) => {
+    setPageNumber(selected);
+  }
+  return (
+    <>
+      {movieCards}
+
+      <div style={{ bottom: 0, left: 0, right: 0 }}>
+        <ReactPaginate 
+        previousLabel={"previous"}
+        nextLabel={"next"}
+        breaklabel={'...'}
+        pageCount={pageCount}
+        marginPagesDiaplayed={2}
+        pageRangeDisplayed={3}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination"}
+        pageLinkClassName={"page-link"}
+        previousLinkClassName={"page-link"}
+        nextLinkClassName={"page-link"}
+        pageClassName={"page-item"}
+        breakClassName={"page-item"}
+        breakLinkClassName={"page-link"}
+      />
+      </div>
+    </> 
+        
+ );
 };
 
 export default MovieList;
--- movies/src/components/peopleList/index.js
+++ movies/src/components/peopleList/index.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React,{ useState} from "react";
 import Person from "../peopleCard"
 import Grid from "@mui/material/Grid";
+import ReactPaginate from "react-paginate";
 
 const PeopleList = ( {people, action }) => {
-  let peopleCards = people.map((p) => (
+  const [pageNumber, setPageNumber] = useState(0);
+  const peoplePerPage = 10;
+  const pagesVisited = pageNumber*peoplePerPage;
+  const peopleCards = people.slice(pagesVisited, pagesVisited + peoplePerPage).map((p) => {
+    return (
     <Grid key={p.id} item xs={12} sm={6} md={4} lg={3} xl={2}>
       <Person key={p.id} person={p} action={action} />
     </Grid>
-  ));
-  return peopleCards;
+  );
+});
+const pageCount = Math.ceil(people.length / peoplePerPage);
+  const handlePageClick = ({selected}) => {
+    setPageNumber(selected);
+  }
+  return (
+    <>
+      {peopleCards}
+      
+      <div style={{ bottom: 0, left: 100, right: 0 }}>
+        <ReactPaginate 
+        previousLabel={"previous"}
+        nextLabel={"next"}
+        pageCount={pageCount}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination "}
+        pageLinkClassName={"page-link"}
+        previousLinkClassName={"page-link"}
+        nextLinkClassName={"page-link"}
+        pageClassName={"page-item"}
+      />
+      </div>
+    </> 
+        
+ );
 };
 
 export default PeopleList;
